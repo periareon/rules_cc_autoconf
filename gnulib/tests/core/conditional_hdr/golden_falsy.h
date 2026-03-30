2862f34db309eea4a8053241ded909b768b79ec0
Andre Brisco <91817010+abrisco@users.noreply.github.com>
2026-03-30 07:14:32 -0700

    Added `gnulib_conditional_hdr` rule  (#103)

---
+++ gnulib/tests/core/conditional_hdr/golden_falsy.h
@@ -0,0 +1,11 @@
+#if 1
+#include_next <test.h>
+#else
+/* Template for conditional_hdr test */
+#ifndef _GL_TEST_HEADER_H
+#define _GL_TEST_HEADER_H
+
+#define TEST_VALUE 42
+
+#endif /* _GL_TEST_HEADER_H */
+#endif
